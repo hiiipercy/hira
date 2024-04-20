c01ef221724b99f603baa2acf2374250c0a45856
Eshaq Miah <129651038+hiiipercy@users.noreply.github.com>
2024-04-20 22:04:16 +0600

    About.jsx

--- src/About.jsx
+++ src/About.jsx
@@ -13,7 +13,7 @@ export default function About(){
             </div>
         <div className="about-body" >
             <div className="bio_left">
-                <img src="./cv2.png" alt="about-img" />
+                <img src="./cv_pic.jpg" alt="about-img" />
             </div>
             <div className="bio_right">
                 <h3>A story of good</h3>
